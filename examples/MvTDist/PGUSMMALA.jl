4687b48e2a997d755621f08aeeee1f22d29242b3
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-07-19 23:18:44 +0100

    Example 1 (t-dist): computational efficiency

---
+++ examples/MvTDist/PGUSMMALA.jl
@@ -0,0 +1,57 @@
+using Distributions
+using Lora
+using PGUManifoldMC
+
+plogtarget(p::Vector, v::Vector) = logpdf(MvTDist(50., [1., 2.], [1. 0.; 0. 1.]), p)
+
+p = BasicContMuvParameter(
+  :p,
+  logtarget=plogtarget,
+  nkeys=1,
+  autodiff=:forward,
+  order=2
+)
+
+model = likelihood_model([p], isindexed=false)
+
+# Simulation 01
+
+sampler = PGUSMMALA(
+  1.,
+  identitymala=false,
+  # update=(sstate, i) -> rand_update!(sstate, i, 0.75),
+  update=(sstate, i, tot) -> rand_exp_decay_update!(sstate, i, tot),
+  transform=H -> softabs(H, 1000.),
+  initupdatetensor=(true, false)
+)
+
+mcrange = BasicMCRange(nsteps=110000, burnin=10000)
+
+v0 = Dict(:p=>[-1., 1.])
+
+outopts = Dict{Symbol, Any}(:monitor=>[:value, :logtarget, :gradlogtarget, :tensorlogtarget], :diagnostics=>[:accept])
+
+job = BasicMCJob(
+  model,
+  sampler,
+  mcrange,
+  v0,
+  tuner=AcceptanceRateMCTuner(0.6, score=x -> logistic_rate_score(x, 3.), verbose=false),
+  outopts=outopts
+)
+
+tic()
+run(job)
+runtime = toc()
+
+chain = output(job)
+
+ppostmean = mean(chain)
+
+ess(chain, vtype=:bm)
+
+ess(chain, vtype=:bm)/runtime
+
+# Float64[ess(vec(chain.value[i, :]), θ[i], pvar[i], chain.n) for i in 1:n]
+
+acceptance(chain)
---
+++ examples/MvTDist/SMMALA.jl
@@ -0,0 +1,50 @@
+using Distributions
+using Lora
+using PGUManifoldMC
+
+plogtarget(p::Vector, v::Vector) = logpdf(MvTDist(50., [1., 2.], [1. 0.; 0. 1.]), p)
+
+p = BasicContMuvParameter(
+  :p,
+  logtarget=plogtarget,
+  nkeys=1,
+  autodiff=:forward,
+  order=2
+)
+
+model = likelihood_model([p], isindexed=false)
+
+# Simulation 01
+
+sampler = SMMALA(1., softabs)
+
+mcrange = BasicMCRange(nsteps=110000, burnin=10000)
+
+v0 = Dict(:p=>[-1., 1.])
+
+outopts = Dict{Symbol, Any}(:monitor=>[:value, :logtarget, :gradlogtarget, :tensorlogtarget], :diagnostics=>[:accept])
+
+job = BasicMCJob(
+  model,
+  sampler,
+  mcrange,
+  v0,
+  tuner=AcceptanceRateMCTuner(0.6, score=x -> logistic_rate_score(x, 3.), verbose=false),
+  outopts=outopts
+)
+
+tic()
+run(job)
+runtime = toc()
+
+chain = output(job)
+
+ppostmean = mean(chain)
+
+ess(chain, vtype=:bm)
+
+ess(chain, vtype=:bm)/runtime
+
+# Float64[ess(vec(chain.value[i, :]), θ[i], pvar[i], chain.n) for i in 1:n]
+
+acceptance(chain)

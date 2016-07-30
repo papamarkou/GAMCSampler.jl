725d9e2b311438680101f87b303cbed990edeb6e
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-07-30 23:22:56 +0100

    Script towards tabulating mala output for logistic ex

--- examples/logistic/mala/table.jl
+++ examples/logistic/mala/table.jl
@@ -1,7 +1,23 @@
-ppostmean = mean(chain)
+using Lora
 
-ess(chain, vtype=:bm)
+DATADIR = "data"
 
-ess(chain, vtype=:bm)/runtime
+npars = 4
 
-acceptance(chain)
+nchains = 10
+nmcmc = 110000
+nburnin = 10000
+
+ratio = Array(Float64, nchains)
+essizes = Array(Float64, nchains, npars)
+results = Dict{Symbol, Any}()
+
+for i in 1:nchains
+  ratio[i] = acceptance(readdlm(joinpath(DATADIR, "diagnostics"*lpad(string(i), 2, 0)*".csv"), ',', Bool))
+
+  essizes[i, :] = ess(readdlm(joinpath(DATADIR, "chain"*lpad(string(i), 2, 0)*".csv"), ',', Float64), 2)
+end
+
+results[:rate] = mean(ratio)
+
+results[:time] = mean(readdlm(joinpath(DATADIR, "times.csv"), ',', Float64))

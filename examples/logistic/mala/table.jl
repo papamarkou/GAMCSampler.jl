3bb4a72e8994c0a50ab2e5fa25482c021425e27a
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-07-31 00:35:16 +0100

    Finished script for tabulating mala out for logistic ex

--- examples/logistic/mala/table.jl
+++ examples/logistic/mala/table.jl
@@ -1,6 +1,7 @@
 using Lora
 
 DATADIR = "data"
+OUTDIR = "output"
 
 npars = 4
 
@@ -14,10 +15,18 @@ results = Dict{Symbol, Any}()
 
 for i in 1:nchains
   ratio[i] = acceptance(readdlm(joinpath(DATADIR, "diagnostics"*lpad(string(i), 2, 0)*".csv"), ',', Bool))
-
   essizes[i, :] = ess(readdlm(joinpath(DATADIR, "chain"*lpad(string(i), 2, 0)*".csv"), ',', Float64), 2)
+
+  println("Iteration ", i, " of ", nchains, " completed")
 end
 
 results[:rate] = mean(ratio)
-
+results[:ess] = mean(essizes, 1)
 results[:time] = mean(readdlm(joinpath(DATADIR, "times.csv"), ',', Float64))
+results[:efficiency] = minimum(results[:ess])/results[:time]
+
+writedlm(
+  joinpath(OUTDIR, "malasummary.txt"),
+  round(Float64[results[:rate] results[:ess] results[:time] results[:efficiency]], 2),
+  " & "
+)

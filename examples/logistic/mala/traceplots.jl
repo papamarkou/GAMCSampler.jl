90543c5c1a7df5a6d4dd497219ccccadf34dd93c
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-07-31 04:37:29 +0100

    Added mala traceplot for logit ex (gadfly)

---
+++ examples/logistic/mala/traceplots.jl
@@ -0,0 +1,47 @@
+using Gadfly
+using Lora
+
+DATADIR = "data"
+OUTDIR = "output"
+
+npars = 4
+
+nchains = 10
+nmcmc = 110000
+nburnin = 10000
+npostburnin = nmcmc-nburnin
+
+ci = 5
+pi = 2
+
+chains = readdlm(joinpath(DATADIR, "chain"*lpad(string(ci), 2, 0)*".csv"), ',', Float64)
+
+chainmean = mean(chains[pi, :])
+
+traceplot = plot(
+  layer(
+    x -> chainmean,
+    0,
+    npostburnin,
+    Geom.line,
+    Theme(default_color=colorant"red")
+  ),
+  layer(
+    x=collect(1:npostburnin),
+    y=chains[pi, :],
+    Geom.line
+  ),
+  Coord.Cartesian(ymin=-2., ymax=3.),
+  Guide.xticks(ticks=[0, npostburnin/2, npostburnin]),
+  Theme(
+    major_label_font_size=14pt,
+    minor_label_font_size=12pt,
+    grid_line_width=0pt,
+    panel_stroke=colornt"grey"
+    ),
+  Guide.xlabel(""),
+  Guide.ylabel(""),
+  Guide.title("")
+)
+
+draw(PDF(joinpath(OUTDIR, "logit_mala_traceplot.pdf"), 14cm, 7cm), traceplot)

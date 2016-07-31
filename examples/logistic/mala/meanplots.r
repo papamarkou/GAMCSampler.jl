0bb691bff7059bacadc2f65d8439ab54b173e83b
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-07-31 21:50:58 +0100

    Started migrated plots to R for better control+quality

--- examples/logistic/mala/meanplots.r
+++ examples/logistic/mala/meanplots.r
@@ -1,4 +1,31 @@
 library(data.table)
+library(stringr)
 
-x <- t(fread("data/chain10.csv", sep=",", header=FALSE))
-mean(x[, 2])
+DATADIR <- "data"
+OUTDIR <- "output"
+
+npars <- 4
+
+nchains <- 10
+nmcmc <- 110000
+nburnin <- 10000
+npostburnin <- nmcmc-nburnin
+
+nmeans <- 10000
+ci <- 6
+pi <- 2
+
+chains <- t(fread(file.path(DATADIR, paste("chain", str_pad(ci, 2, pad="0"), ".csv", sep="")), sep=",", header=FALSE))
+
+chainmean <- mean(chains[, pi])
+
+submeans <- vector(mode="numeric", length=nmeans)
+for (i in 1:nmeans) {
+  submeans[i] <- mean(chains[1:i, pi])
+}
+
+plot(
+  1:nmeans,
+  submeans,
+  type="l"  
+)
